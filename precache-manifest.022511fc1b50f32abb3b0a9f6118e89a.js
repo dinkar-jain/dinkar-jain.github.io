4a047b28af324036b1e3cd94caa4a289a7233e84
Dinkar Jain <jaindinkar@ymail.com>
2022-10-22 18:59:26 +0530

    Updates

---
+++ precache-manifest.022511fc1b50f32abb3b0a9f6118e89a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427262f6ebf64f013a534e2490b9994c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc2a2d9fb3c35466cd4f",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "1b6b0dd9911691574146",
+    "url": "/static/css/main.d837075c.chunk.css"
+  },
+  {
+    "revision": "dc2a2d9fb3c35466cd4f",
+    "url": "/static/js/2.b33c3ad1.chunk.js"
+  },
+  {
+    "revision": "0297ac78c823ef9b172e38637e06e92e",
+    "url": "/static/js/2.b33c3ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6b0dd9911691574146",
+    "url": "/static/js/main.3ac913d7.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  },
+  {
+    "revision": "f0f76d1af36c8480de6ac304080d6d6c",
+    "url": "/static/media/myProfile.f0f76d1a.png"
+  },
+  {
+    "revision": "384b8d91beb0987ea74edc449073a6bb",
+    "url": "/static/media/p1.384b8d91.jpg"
+  },
+  {
+    "revision": "a63df0d89eee2f7cc46e34b2fcc6a049",
+    "url": "/static/media/p1.a63df0d8.jpg"
+  },
+  {
+    "revision": "f57ae4adefa0a88dba407671559800df",
+    "url": "/static/media/p1.f57ae4ad.jpg"
+  },
+  {
+    "revision": "fd29971030f077014b86969a7d191ef1",
+    "url": "/static/media/p1.fd299710.png"
+  },
+  {
+    "revision": "61c27d2cd39a713f7829422c3d9edcc7",
+    "url": "/static/media/robots.61c27d2c.txt"
+  }
+]);
